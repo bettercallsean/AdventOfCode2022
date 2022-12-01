98d103f1cc228c4912725813a55c2dd39f995e7b
Sean Edwards <hello@seanedwards.dev>
2022-12-01 13:14:56 +0000

    Solution for day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,32 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
+    private readonly List<int> _caloryCount = new List<int>();
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var count = 0;
+        foreach (var caloryCount in _input)
+        {
+            if (string.IsNullOrWhiteSpace(caloryCount))
+            {
+                _caloryCount.Add(count);
+                count = 0;
+                continue;
+            }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+            count += int.Parse(caloryCount);
+        }
+
+        _caloryCount.Sort();
+        return new(_caloryCount[_caloryCount.Count - 1].ToString());
+    }
+
+    public override ValueTask<string> Solve_2() => new((_caloryCount[_caloryCount.Count - 1] + _caloryCount[_caloryCount.Count - 2] + _caloryCount[_caloryCount.Count - 3]).ToString());
 }
